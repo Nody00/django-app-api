7ed346dc6b1f051ab2ba4d75760c7837122bf907
nody00 <dinokrcicprof@gmail.com>
2025-02-02 18:31:28 +0100

    user creation api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -54,5 +54,5 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload["email"])
+        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
         self.assertFalse(user_exists)
